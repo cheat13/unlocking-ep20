d4d2cf50bf07edc358f7eb87bf8349317e4b3a0e
aphaphon <pui.perspectives@gmail.com>
2018-06-22 20:28:10 +0700

    Prepare code for create new product.

--- mix-coffeeshop-web/Controllers/ProductController.cs
+++ mix-coffeeshop-web/Controllers/ProductController.cs
@@ -11,16 +11,6 @@ namespace mix_coffeeshop_web.Controllers
     [Route("[controller]/[action]")]
     public class ProductController : Controller
     {
-        public static IList<Product> products = new List<Product>
-        {
-            new Product{ Id = 1, Name = "Hot Coffee", Price = 50, ThumbURL = "/images/hot-coffee.png" },
-            new Product{ Id = 2, Name = "Cheese Cake", Price = 60, ThumbURL = "/images/cheese-cake.png" },
-            new Product{ Id = 3, Name = "Chocolate Cake", Price = 70, ThumbURL = "/images/chocolate-cake.png" },
-            new Product{ Id = 4, Name = "Crepe Cake", Price = 80, ThumbURL = "/images/crepe-cake.png" },
-            new Product{ Id = 5, Name = "Ice Coffee", Price = 90, ThumbURL = "/images/ice-coffee.png" },
-            new Product{ Id = 6, Name = "Panna Cotta", Price = 100, ThumbURL = "/images/panna-cotta.png" },
-        };
-
         private IProductRepository repo;
 
         public ProductController(IProductRepository repo)
@@ -45,8 +35,9 @@ namespace mix_coffeeshop_web.Controllers
         [HttpPost]
         public Product CreateNewProduct([FromBody]Product product)
         {
-            product.Id = products.Count + 1;
-            products.Add(product);
+            var products = repo.GetAllProducts();
+            product.Id = products.Count() + 1;
+            repo.CreateNewProduct(product);
             return product;
         }
 
